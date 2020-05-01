689f811f4d6220324dfc8e658235c961ea359336
amirmaula <amirmaula@outlook.com>
2020-05-01 17:04:16 +0100

    Returning eqivalent youtube download link

--- dotify.py
+++ dotify.py
@@ -1,3 +1,20 @@
-from spotifyHandle import stripTitlefromLink
+from youtube_search import YoutubeSearch  as yts
 
-stripTitlefromLink("spotify:track:64T3mDRHqtchar88NZ4Bnf")
+from spotifyHandle import pull_track_artist
+
+#Return track name and track artist(s) from spotify link
+
+user_SpotifyURI = input("Enter a Spotify URI: ")
+if user_SpotifyURI == "i":
+    user_SpotifyURI = "spotify:track:64T3mDRHqtchar88NZ4Bnf"
+
+currentTrack = pull_track_artist(user_SpotifyURI)
+print(currentTrack)
+
+yts_results = yts(str(currentTrack), max_results=10).to_dict()
+print(yts_results)
+
+#Take first result
+print(type(yts_results))
+videoLink =  ('https://www.youtube.com' + yts_results[0]['link'])
+print(videoLink)
--- spotifyHandle.py
+++ spotifyHandle.py
@@ -1,29 +1,37 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+
 from api_keys import *
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def stripTitlefromLink(spotify_URI):
+def pull_track_artist(spotify_URI):
     """Provide a Spotify link as string
     and return the Track - Artist Name"""
 
-    track_info = sp.track(spotify_URI)
+    try:
+        track_info = sp.track(spotify_URI)
+        # For more than 1 artist, get the first two artists
+        if len(track_info.get("artists")) > 1:
+            track_artist = str(track_info.get("artists")[0].get("name")) + " " + str(
+                track_info.get("artists")[1].get("name"))
+        else:
+            # Single artist
+            track_artist = track_info.get("artists")[0].get("name")
 
-    # For more than 1 artist, get the first two artists
-    if len(track_info.get("artists")) > 1:
-        track_artist = str(track_info.get("artists")[0].get("name"))+" "+str(track_info.get("artists")[1].get("name"))
-    else:
-        # Single artist
-        track_artist = track_info.get("artists")[0].get("name")
+        track_name = track_info.get("name")
+        track_artist = track_artist
+        track_album = track_info.get("album").get("name")
+        track_explicit = track_info.get("explicit")
 
-    track_name = track_info.get("name")
-    track_artist = track_artist
-    track_album = track_info.get("album").get("name")
-    track_explicit = track_info.get("explicit")
+        # print("Track Name : "+track_name+
+        #     #     #       "\n Artist: "+ str(track_artist)+
+        #     #     #       "\n Album: "+ track_album+
+        #     #     #       "\n Explicit: "+str(track_explicit))
 
-    print("Track Name : "+track_name+
-          "\n Artist: "+ str(track_artist)+
-          "\n Album: "+ track_album+
-          "\n Explicit: "+str(track_explicit))
+        return track_name, track_artist
+    except:
+        print("An error occurred as follows; ")
+        return None
+
